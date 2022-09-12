770be35d840b09c14f0942671bd065cec83a0b0d
mkapmagday <83943725+mkapmagday@users.noreply.github.com>
2022-09-12 15:09:03 +0800

    Update Project.vue

--- src/views/Project.vue
+++ src/views/Project.vue
@@ -1,3 +1,14 @@
 <template>
-    <h1>test</h1>
+    <v-card-text> 
+            The project was made, using axios and vue
+            <br><br>
+            Axios is an HTTP client library based on 
+            promises. It makes sending asynchronous HTTP 
+            requests to REST endpoints easier and helps you perform CRUD operations. This REST endpoint/API could be an external API like the Google API, GitHub API, and so on – or it could be your own backend Node.
+            <br><br>
+            Vuetify is a full-featured UI framework built on Vue.js. The project's goal is to give developers the tools
+            they need to create rich and engaging user experiences. Unlike other frameworks, Vuetify is built from the
+            ground up to be simple to learn and rewarding to master, with hundreds of carefully crafted Material Design
+            components.
+        </v-card-text>
 </template>
